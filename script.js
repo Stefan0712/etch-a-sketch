82d1ce50622043dab1461d85045620ac6b7d98a4
Stefan <s.vladulescu@gmail.com>
2022-03-27 13:00:33 +0300

    Fixed it so it will use the same ammount of pixels no matter how many squares are

--- script.js
+++ script.js
@@ -8,9 +8,10 @@ function reset(){
     var newDivs = document.createElement("div");
     newDivs.classList.add('main-container');
     body.appendChild(newDivs);
-    //asks for user input and checks if the number in between 1-100
+    //asks for user input 
     var input = window.prompt("Please choose how many squares you want: ");
     checkInput(input);
+    //a function that checks the user input and ask again until the right input is entered
     function checkInput(input){
     if(input>100){
         alert("Your number should be max 100");
@@ -25,16 +26,19 @@ function reset(){
 
 
 function addDivs(num){
+    const subDivs = document.querySelector(".subDiv");
+    let maxWidth = 900/num;
     const mainContainer = document.querySelector(".main-container");
-    let maxWidth = num*28;
-    mainContainer.style.cssText = `max-width: ${maxWidth}px`;
     for(let i=0;i<num;i++){
         for(let j=0;j<num;j++){
 
             //creates a subDiv
             const content = document.createElement('div');
             content.classList.add('subDiv');
+            content.style.cssText = `width: ${maxWidth}px; height: ${maxWidth}px`;
             mainContainer.appendChild(content);
+            
+
         }
     }
     
