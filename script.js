69a9ceb9771850eb0b9d4ead013630b5d6624e64
Stefan <s.vladulescu@gmail.com>
2022-03-27 12:51:45 +0300

    Added a checkInput function to be sure the right number is introduced

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
 var num = 16;
 function reset(){
+    //delete the entire main-container
     var oldDivs = document.querySelector(".main-container");
     oldDivs.parentNode.removeChild(oldDivs);
+    //then it adds a new empty main-container
     var body = document.querySelector(".body");
     var newDivs = document.createElement("div");
     newDivs.classList.add('main-container');
     body.appendChild(newDivs);
-
+    //asks for user input and checks if the number in between 1-100
     var input = window.prompt("Please choose how many squares you want: ");
+    checkInput(input);
+    function checkInput(input){
     if(input>100){
         alert("Your number should be max 100");
         var input = window.prompt("Please choose how many squares you want: ");
-
-    }
+        checkInput(input);
+        } else {
+            return input;
+        }
+    }   
     addDivs(input);
 }
 
@@ -24,7 +31,7 @@ function addDivs(num){
     for(let i=0;i<num;i++){
         for(let j=0;j<num;j++){
 
-            
+            //creates a subDiv
             const content = document.createElement('div');
             content.classList.add('subDiv');
             mainContainer.appendChild(content);
